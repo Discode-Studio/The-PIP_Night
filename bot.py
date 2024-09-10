da285bf625aebf6ae64700ce854b83f67cd3b490
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-09-10 12:20:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import yt_dlp
+import wavelink
 import os
 
 # Définir les intents
@@ -13,42 +13,72 @@ intents.voice_states = True
 # Configuration du bot avec les intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Fonction pour obtenir l'URL du flux audio de YouTube
-def get_youtube_audio_url(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'noplaylist': True,
-        'quiet': True,
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        return info['formats'][0]['url']
+# Fonction de démarrage du bot
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    bot.loop.create_task(connect_nodes())
 
-# Commande pour jouer la musique
+# Connexion aux nodes Lavalink
+async def connect_nodes():
+    await bot.wait_until_ready()
+    node = wavelink.Node(uri='http://localhost:2333', password='youshallnotpass')
+    await wavelink.NodePool.connect(node)
+
+# Commande pour rejoindre un canal vocal
 @bot.command()
-async def play(ctx, url):
+async def join(ctx):
     if ctx.author.voice:
         voice_channel = ctx.author.voice.channel
-        if ctx.voice_client is None:
-            vc = await voice_channel.connect()
+        vc = ctx.voice_client
+        
+        if not vc:
+            await voice_channel.connect(cls=wavelink.Player)
+            await ctx.send(f'Joined {voice_channel}!')
         else:
-            vc = ctx.voice_client
-
-        audio_url = get_youtube_audio_url(url)
-        if not vc.is_playing():
-            vc.play(discord.FFmpegPCMAudio(audio_url), after=lambda e: print('Player error: %s' % e) if e else None)
-            await ctx.send(f'Now playing: {url}')
-        else:
-            await ctx.send('Already playing audio.')
+            await ctx.send("Already in a voice channel.")
     else:
-        await ctx.send("You are not connected to a voice channel.")
+        await ctx.send("You need to join a voice channel first.")
+
+# Commande pour jouer une piste à partir d'un lien YouTube
+@bot.command()
+async def play(ctx, url: str):
+    vc = ctx.voice_client
+    if not vc:
+        await ctx.send("Bot is not in a voice channel. Use !join to make the bot join a channel.")
+        return
+    
+    # Charger et jouer une piste YouTube
+    track = await wavelink.YouTubeTrack.search(query=url, return_first=True)
+
+    # Lancer la piste dans le canal vocal
+    await vc.play(track)
+    await ctx.send(f'Now playing: {track.title}')
+
+# Commande pour rechercher une piste YouTube
+@bot.command()
+async def search(ctx, *, query: str):
+    vc = ctx.voice_client
+    if not vc:
+        await ctx.send("Bot is not in a voice channel. Use !join to make the bot join a channel.")
+        return
+    
+    # Recherche des pistes YouTube
+    results = await wavelink.YouTubeTrack.search(query=query)
+    if results:
+        track = results[0]  # Prendre la première piste trouvée
+        await vc.play(track)
+        await ctx.send(f'Now playing: {track.title}')
+    else:
+        await ctx.send("No results found.")
 
 # Commande pour arrêter la musique
 @bot.command()
 async def stop(ctx):
-    if ctx.voice_client:
-        await ctx.voice_client.disconnect()
-        await ctx.send('Stopped and disconnected from voice channel.')
+    vc = ctx.voice_client
+    if vc:
+        await vc.disconnect()
+        await ctx.send('Stopped and disconnected from the voice channel.')
     else:
         await ctx.send("I'm not connected to a voice channel.")
 
