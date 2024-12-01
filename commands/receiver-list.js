307ac42764cf8c64f593d325e87f0fe51465247a
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-12-01 19:46:42 +0100

    Update receiver-list.js

--- commands/receiver-list.js
+++ commands/receiver-list.js
@@ -3,7 +3,12 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('receiver-list')
-        .setDescription('Provides a list of receiver platforms with links.'),
+        .setDescription('Provides a list of receiver platforms with links.')
+        .addBooleanOption(option =>
+            option
+                .setName('ephemeral')
+                .setDescription('Whether the response should be ephemeral (visible only to you).')
+        ),
     async execute(interaction) {
         const response = `**Receiver Platforms:**\n\n`
             + `**OpenWebRX:** https://www.receiverbook.de/\n`
@@ -11,6 +16,9 @@ module.exports = {
             + `**KiwiSDR:** http://kiwisdr.com/public/\n`
             + `**WebRX-888:** https://www.rx-888.com/web/rx.html\n`;
 
-        await interaction.reply({ content: response, ephemeral: true });
+        // Récupération de l'option "ephemeral" avec une valeur par défaut (false)
+        const isEphemeral = interaction.options.getBoolean('ephemeral') ?? false;
+
+        await interaction.reply({ content: response, ephemeral: isEphemeral });
     },
 };
